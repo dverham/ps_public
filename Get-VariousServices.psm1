9a30198fb3785b0df9346046856aecfb772acfb3
dverham <49904974+dverham@users.noreply.github.com>
2019-10-31 14:11:30 +0100

    Create Get-VariousServices.psm1

---
+++ Get-VariousServices.psm1
@@ -0,0 +1,70 @@
+Function Get-FileName($InitialDirectory){
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+
+  $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+  $OpenFileDialog.initialDirectory = $initialDirectory
+  $OpenFileDialog.filter = "Plain Text (*.txt) | *.txt"
+  $OpenFileDialog.ShowDialog() | Out-Null
+  $OpenFileDialog.FileName
+}
+
+function Get-NonMicrosoftServices (){
+    # Vraag de non-Microsoft services op van remote computers.
+    # De functie vereist de Get-FileName functie.
+    $FilePath = Get-FileName 
+    $InputServers = Get-Content $FilePath 
+    [System.Collections.ArrayList]$RemoteOutput = @()
+    $RemoteOutput = foreach ($Server in $InputServers) {
+        Invoke-Command -ComputerName $Server -ScriptBlock {
+            $Services = Get-WmiObject Win32_Service -Property Name,DisplayName,PathName | Select Name,DisplayName,PathName
+            $ServiceList = New-Object System.Collections.ArrayList
+            foreach ($Service in $Services) {
+                Try {
+                    $Path = $Service.Pathname.tostring().replace('"','')
+                    $LegalCopyRight = ([System.Diagnostics.FileVersionInfo]::GetVersionInfo($Path)).legalcopyright
+                if ($LegalCopyRight -notlike "*Microsoft*") {
+                        $Object = New-Object psobject -Property @{
+                            Name        = $Service.Name
+                            DisplayName = $Service.DisplayName
+                            PathName    = $Service.PathName
+                            Server      = $ENV:COMPUTERNAME
+                        }
+                        $ServiceList += $Object
+                    }
+                }
+                catch {}
+            }
+        $ServiceList | ft -AutoSize
+        }
+    }
+}
+
+function Get-MicrosoftServices (){
+    # Vraag de non-Microsoft services op van remote computers.
+    # De functie vereist de Get-FileName functie.
+    $FilePath = Get-FileName 
+    $InputServers = Get-Content $FilePath 
+    [System.Collections.ArrayList]$RemoteOutput = @()
+    $RemoteOutput = foreach ($Server in $InputServers) {
+        Invoke-Command -ComputerName $Server -ScriptBlock {
+            $Services = Get-WmiObject Win32_Service -Property Name,DisplayName,PathName | Select Name,DisplayName,PathName
+            $ServiceList = New-Object System.Collections.ArrayList
+            foreach ($Service in $Services) {
+                Try {
+                    $Path = $Service.Pathname.tostring().replace('"','')
+                    $LegalCopyRight = ([System.Diagnostics.FileVersionInfo]::GetVersionInfo($Path)).legalcopyright
+                if ($LegalCopyRight -like "*Microsoft*") {
+                        $Object = New-Object psobject -Property @{
+                            Name        = $Service.Name
+                            DisplayName = $Service.DisplayName
+                            PathName    = $Service.PathName
+                            Server      = $ENV:COMPUTERNAME
+                        }
+                        $ServiceList += $Object
+                    }
+                }
+                catch {}
+            }
+        $ServiceList | ft -AutoSize
+        }
+    }
