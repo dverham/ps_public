359235e12e6ede7dd80b3f72f75b763b40fe445a
dverham <49904974+dverham@users.noreply.github.com>
2020-01-24 15:46:19 +0100

    Delete Update-ExtensionsLocally.ps1

--- Update-ExtensionsLocally.ps1
+++
@@ -1,3 +0,0 @@
-xcopy '\\SERVER\FsrmConfig$\Known_Extensions.txt' 'C:\Scripts\Ransomware' /Y
-$Extensions = Get-Content 'C:\Scripts\Ransomware\Known_Extensions.txt'
-Set-FsrmFileGroup -name "Ransomware_Extensions" -IncludePattern($Extensions)
